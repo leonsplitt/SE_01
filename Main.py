fe1a9198a803e672b3fae484778e8320f5501bd0
Leon Splitt <splittleon@gmail.com>
2022-11-30 17:04:32 +0100

    Add input grid

---
+++ Main.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+#input_grid = [[0,1,0],[0,1,0],[0,1,0]]
+#print(input_grid)
+#input_grid[1][0] = 2
+#print (input_grid)
+
+# Initial input
+input_grid = np.array([[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0]])
+print(input_grid)
+
+# Create output grid
+output_grid = np.array([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]])
+
+# Count neighbours for each cell
+
+
+
+
+
+
+
+
+# Print new/updated grid
